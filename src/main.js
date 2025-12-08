249ce1ee639cc2ff519ccff832dbd3c94ce40e9a
黒田 <kamyu9818482688@gmail.com>
2025-12-08 16:09:41 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -1,29 +1,27 @@
 // 自分が使いたい関数だけ、必要な SDK から読み込もう
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getFirestore, collection, getDocs, addDoc}
+import { getFirestore, collection, getDocs, addDoc, query, orderBy}
 from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
-import {query, orderBy } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js"
 
 // Firebase設定
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ,
-  messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Firebaseを使える状態にセットアップ
 const app = initializeApp(firebaseConfig);
-
-//Cloud Firestoreの初期化
-//dbでデータの読み書きができるようになる
 const db = getFirestore(app);
 
 //Cloud Firestoreから取得したデータを表示、
 //fetch=データをサーバーから取得するための関数
 const fetchHistoryData = async () => {
+  const tableBody = document.getElementById("js-history");
+  if (!tableBody) return;
   let tags = "";
 
 //データを新しい順にならべる
@@ -31,13 +29,14 @@ const q = query(collection(db, "report"), orderBy("date", "desc"));
 
 //reportコレクションのデータを取得
 const querySnapshot = await getDocs(q);
+console.log("querySnapshot:", querySnapshot.size);
 
 //データをテーブルの表の形式に合わせてHTMLに挿入
   querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data()}`);
+    const data = doc.data();
     tags += `
       <tr>
-        <td>${doc.data().date.toDate().toLocaleString()}</td>
+        <td>${new Date(data.date).toLocaleString()}</td>
         <td>${doc.data().name}</td>
         <td>${doc.data().work}</td>
         <td>${doc.data().comment}</td>
@@ -59,14 +58,15 @@ const submitData = async (e) => {
     const formData = new FormData(e.target);
 
     try{
-      const docRef = await addDoc(collection(db,"report"),{
+      await addDoc(collection(db,"report"),{
         date: new Date(),
         name: formData.get("name"),
         work: formData.get("work"),
         comment: formData.get("comment")
       });
-      console.log("Document written with ID:" ,docRef.id);
-      fetchHistoryData();
+
+      alert("登録しました！");
+
     } catch (e) {
       console.log("Error adding document:", e);
     }
@@ -74,8 +74,7 @@ const submitData = async (e) => {
 
 //CloudFirestoreにデータを送信する&登録しましたアラート
 if(document.getElementById("js-form")) {
-   document.getElementById("js-form").addEventListener("submit",(e) => {e.preventDefault();
-   submitData(e);
-   alert("登録しました！");
-  });
+   document.getElementById("js-form").addEventListener("submit",(e) => {
+    submitData(e);
+   });
 }
